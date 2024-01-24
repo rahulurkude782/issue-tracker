5302d650ef30f1a3980012eeb64c8d38bb3a92b7
rahulurkude782 <rahulurkude782@gmail.com>
2024-01-24 16:33:57 +0530

    Markdown editor and React hookform added

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,61 @@
 "use client";
 
-import { Button, TextArea, TextField, TextFieldInput } from "@radix-ui/themes";
-import React from "react";
+import "easymde/dist/easymde.min.css";
+import {
+  Button,
+  Callout,
+  TextArea,
+  TextField,
+  TextFieldInput,
+} from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useState } from "react";
+import { AiFillInfoCircle } from "react-icons/ai";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
+
+  const onFormSubmit = async (data: IssueForm) => {
+    try {
+      const response = await axios.post("/api/issues", data);
+      console.log(response);
+    } catch (error) {
+      setError("An Unexpected error has occurred.");
+      setTimeout(() => setError(""), 3000);
+    }
+  };
+
   return (
     <div className="max-w-xl space-y-3">
-      <TextField.Root>
-        <TextField.Input placeholder="Title" />
-      </TextField.Root>
-      <TextArea placeholder="Description" />
-      <Button>Submit</Button>
+      {error && (
+        <Callout.Root>
+          <Callout.Icon>
+            <AiFillInfoCircle />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className=" space-y-3" onSubmit={handleSubmit(onFormSubmit)}>
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
     </div>
   );
 };
