353e69917903b651e012ea706970beeeba3a89c0
rahulurkude782 <rahulurkude782@gmail.com>
2024-01-25 14:59:12 +0530

    Issues Details Page added

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import prisma from "../../../prisma/client";
+interface Props {
+  params: { id: string };
+}
 
-const IssueDetailsPage = () => {
-  return <div>IssueDetailsPage</div>;
+const IssueDetailsPage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+  return (
+    <div>
+      <p>{issue?.title}</p>
+      <p>{issue?.status}</p>
+      <p>{issue?.createdAt.toDateString()}</p>
+      <p>{issue?.description}</p>
+    </div>
+  );
 };
 
 export default IssueDetailsPage;
