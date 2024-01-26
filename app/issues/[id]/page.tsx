95908db8598c62efa9e040d1dd30fc0bb50adaa5
rahulurkude782 <rahulurkude782@gmail.com>
2024-01-26 15:52:28 +0530

    Add Edit Button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,5 +1,5 @@
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import ReactMarkdown from "react-markdown";
 import prisma from "../../../prisma/client";
 interface Props {
@@ -14,17 +14,22 @@ const IssueDetailsPage = async ({ params }: Props) => {
   });
 
   return (
-    <div>
-      <Heading as="h2">{issue?.title}</Heading>
-      <Flex gap={"3"} my={"2"}>
-        <IssueStatusBadge status={issue!.status} />
+    <Grid columns={{ initial: "1", md: "2" }} gap={"5"}>
+      <Box>
+        <Heading as="h2">{issue?.title}</Heading>
+        <Flex gap={"3"} my={"2"}>
+          <IssueStatusBadge status={issue!.status} />
 
-        <Text as="p">{issue?.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose" mt={"4"}>
-        <ReactMarkdown>{issue?.description}</ReactMarkdown>
-      </Card>
-    </div>
+          <Text as="p">{issue?.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose" mt={"4"}>
+          <ReactMarkdown>{issue?.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>Edit Issue</Button>
+      </Box>
+    </Grid>
   );
 };
 
