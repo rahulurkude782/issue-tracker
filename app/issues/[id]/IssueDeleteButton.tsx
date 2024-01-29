118c9a50f07924e89dde8d7f186d4e4e6da02f57
rahulurkude782 <rahulurkude782@gmail.com>
2024-01-29 15:47:22 +0530

    Add Issue Delete Alert Dailogue Box

--- app/issues/[id]/IssueDeleteButton.tsx
+++ app/issues/[id]/IssueDeleteButton.tsx
@@ -1,11 +1,32 @@
-import { Button } from "@radix-ui/themes";
-import React from "react";
+"use client";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 
 const IssueDeleteButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color="red" className="w-full">
-      Delete Issue
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red" className="w-full">
+          Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content className="space-y-4">
+        <AlertDialog.Title>Confirm Delete</AlertDialog.Title>
+        <AlertDialog.Description>
+          Do you really want to delete this issue.This action can&apos;t be
+          undone.
+        </AlertDialog.Description>
+        <Flex gap={"4"}>
+          <AlertDialog.Cancel>
+            <Button color="gray" variant="soft">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Delete Issue</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
