90fa1575c384cb2be004f326499cc7feaad04237
rahulurkude782 <rahulurkude782@gmail.com>
2024-02-03 17:03:11 +0530

    Build Issue Status Summery Component

---
+++ app/IssueSummery.tsx
@@ -0,0 +1,39 @@
+import { Status } from "@prisma/client";
+import { Card, Flex, Text } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
+
+interface Props {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
+
+const IssueSummery = ({ open, inProgress, closed }: Props) => {
+  const cards: {
+    label: string;
+    value: number;
+    status: Status;
+  }[] = [
+    { label: "Open", value: open, status: "OPEN" },
+    { label: "In-progress", value: inProgress, status: "IN_PROGRESS" },
+    { label: "Closed", value: closed, status: "CLOSED" },
+  ];
+
+  return (
+    <Flex gap="4">
+      {cards.map((card) => (
+        <Link key={card.label} href={"/issues/list?status=" + card.status}>
+          <Card className="w-52">
+            <Flex direction="column">
+              <Text className="font-medium text-sm">{card.label}</Text>
+              <Text className="font-bold">{card.value}</Text>
+            </Flex>
+          </Card>
+        </Link>
+      ))}
+    </Flex>
+  );
+};
+
+export default IssueSummery;
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,12 @@ import { getServerSession } from "next-auth";
 import Pagination from "./components/Pagination";
 import { useSearchParams } from "next/navigation";
 import LatestIssues from "./LatestIssues";
+import IssueSummery from "./IssueSummery";
 
 export default async function Home() {
   return (
     <main>
-      <LatestIssues />
+      <IssueSummery open={10} inProgress={90} closed={2} />
     </main>
   );
 }
